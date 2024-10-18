a15a595f371d858660e8ca4732fbcf527d8724bf
Ankit <private.ankit047@gmail.com>
2024-10-18 20:46:45 +0530

    clean

--- new.sh
+++
@@ -1,8 +0,0 @@
-echo "hello world"
-pwd
-ls
-git status
-git add .
-git commit -m "testing shell scriptig"
-git push
-git status
--- shell/new.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/python3
-print("Hellow this is python script and shell ")
