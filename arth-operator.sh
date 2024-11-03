1a18e4e56f3b5322750de5feb8d0bf9fbcad5f4c
Ankit <private.ankit047@gmail.com>
2024-11-03 21:51:54 +0530

    update

---
+++ arth-operator.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+a=5
+b=6
+echo "$((a+b))"
+echo "$((a-b))"
+echo "$((a*b))"
+echo "$((a%b))"
+echo "$((a/b))"
+echo "$((a**b))"
+echo "$((a++))"
+echo "$((++a))"
+echo "$((a--))"
+echo "$((--a))"
+echo "$((a+=3))"
+echo "$((8+=3))"
