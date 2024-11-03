359500ffdeca4d6b168e113b11782f6c992a8678
Ankit <private.ankit047@gmail.com>
2024-11-03 22:00:02 +0530

    functions

---
+++ functions.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+function install(){
+    #### installation code
+echo "installation code1"
+echo "installation code5"
+echo "installation code4"
+echo "installation code3"
+echo "installation code2"
+
+}
+install
+configuration(){
+    # configuration code
+ echo "configcode1"
+ echo "configcode2"
+ echo "configcode3"
+
+}
+configuration
+
+
+function deploy{
+    # deploy code
+
+echo "deploy code1"
+echo "deploy code2"
+echo "deploy code3"
+}
+deploy
