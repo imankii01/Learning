81f270af28b121aade2970d81146c38e9f55e49c
Ankit <private.ankit047@gmail.com>
2024-11-03 22:37:06 +0530

    local

---
+++ local-variable.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+packageName="nginx"
+function install (){
+
+    echo "executing ${FUNCNAME} - START"
+    echo "installing ${1}"
+    echo "executing ${packageName} in ${FUNCNAME}"
+}
+install "testing"
