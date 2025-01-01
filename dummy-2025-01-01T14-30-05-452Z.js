c545acf8707dcfd666c826ad031cbbdf7af24076
Ankit <private.ankit047@gmail.com>
2025-01-01 20:00:05 +0530

    Auto-commit at 1/1/2025, 8:00:05 PM

---
+++ dummy-2025-01-01T14-30-05-452Z.js
@@ -0,0 +1,11 @@
+
+// filepath: /Users/ankit/development/simple-git/repo/learning/dummy-2025-01-01T14-30-05-452Z.js
+function helloWorld() {
+  console.log('Hello, world!');
+}
+
+function add(a, b) {
+  return a + b;
+}
+
+module.exports = { helloWorld, add };
